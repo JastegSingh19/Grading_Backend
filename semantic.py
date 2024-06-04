894b9082183a223895c023bbc99ee0b39ae62984
JastegSingh19 <104299277+JastegSingh19@users.noreply.github.com>
2024-06-04 23:20:59 +0530

    Update semantic.py

--- semantic.py
+++ semantic.py
@@ -157,5 +157,5 @@ def grade():
     return csv_content, 200, {'Content-Type': 'text/csv'}
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+'''if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')'''
